c56cb20128ee617ceb8d5a00e089f88978a66280
takumi <takumi@indeed.com>
2020-08-06 12:43:41 +0900

    remove thread

--- solver/src/bin/solver.rs
+++ solver/src/bin/solver.rs
@@ -50,10 +50,5 @@ fn run() -> std::io::Result<()> {
 }
 
 fn main() {
-    let _ = ::std::thread::Builder::new()
-        .name("run".to_string())
-        .stack_size(32 * 1024 * 1024)
-        .spawn(run)
-        .unwrap()
-        .join();
+    run().unwrap();
 }
